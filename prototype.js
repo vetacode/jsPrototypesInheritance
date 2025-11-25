bd0cab625bdf8ac9a182eff071341736c73d1e31
vetacode <vetacode@gmail.com>
2025-11-25 14:44:05 +0700

    task 1 completed

--- prototype.js
+++ prototype.js
@@ -157,3 +157,45 @@ for (let props in pisang) {
     console.log(`inherited: ${props}`); //inherited: berair, kulit
   }
 }
+
+/**TASK 1
+ * Working with prototype
+importance: 5
+Here’s the code that creates a pair of objects, then modifies them.
+
+Which values are shown in the process?
+
+let animal = {
+  jumps: null
+};
+let rabbit = {
+  __proto__: animal,
+  jumps: true
+};
+
+alert( rabbit.jumps ); // ? (1)
+
+delete rabbit.jumps;
+
+alert( rabbit.jumps ); // ? (2)
+
+delete animal.jumps;
+
+alert( rabbit.jumps ); // ? (3)
+There should be 3 answers.
+ */
+
+let animal4 = {
+  jumps: null,
+};
+let rabbit4 = {
+  __proto__: animal4,
+  jumps: true,
+};
+console.log(rabbit4.jumps); //true
+
+delete rabbit4.jumps;
+console.log(rabbit4.jumps); //null
+
+delete animal4.jumps;
+console.log(rabbit4.jumps); //undefined
