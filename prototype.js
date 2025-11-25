01f3126a0676229824d6c7513c4e0016e8122263
vetacode <vetacode@gmail.com>
2025-11-25 14:49:44 +0700

    task 2 accomplished

--- prototype.js
+++ prototype.js
@@ -199,3 +199,56 @@ console.log(rabbit4.jumps); //null
 
 delete animal4.jumps;
 console.log(rabbit4.jumps); //undefined
+
+/**TASK 2
+ * Searching algorithm
+importance: 5
+The task has two parts.
+
+Given the following objects:
+
+let head = {
+  glasses: 1
+};
+
+let table = {
+  pen: 3
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2
+};
+
+let pockets = {
+  money: 2000
+};
+Use __proto__ to assign prototypes in a way that any property lookup will follow the path: pockets → bed → table → head. For instance, pockets.pen should be 3 (found in table), and bed.glasses should be 1 (found in head).
+Answer the question: is it faster to get glasses as pockets.glasses or head.glasses? Benchmark if needed.
+ */
+
+let head = {
+  glasses: 1,
+};
+
+let table = {
+  pen: 3,
+  __proto__: head,
+};
+
+let bed = {
+  sheet: 1,
+  pillow: 2,
+  __proto__: table,
+};
+
+let pockets = {
+  money: 2000,
+  __proto__: bed,
+};
+console.log(pockets.pen); // 3
+console.log(bed.glasses); // 1
+console.log(table.money); // undefined
+
+console.log(head.glasses);
+console.log(pockets.glasses); //there is no difference in performance in modern engines
